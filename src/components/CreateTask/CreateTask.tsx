229e2c989785baf28d2d846d88ac98085d5cbf1f
Alejo Gustavo Francomano <alejogustavof@gmail.com>
2025-08-20 11:35:05 +0200

    add tailwind css

--- src/components/CreateTask/CreateTask.tsx
+++ src/components/CreateTask/CreateTask.tsx
@@ -18,15 +18,15 @@ function CreateTask({ setTasks }: CreateTaskProps) {
   } = useCreateTask(setTasks);
 
   return (
-    <div className="create-task">
-      <input type="text" placeholder="Task Title" name="taskTitle" value={title} onChange={(e) => setTitle(e.target.value)} />
-      <textarea placeholder="Task Description" name="taskDescription" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
-      <select name="taskStatus" value={status} onChange={(e) => setStatus(e.target.value as "ready" | "inprogress" | "complete")}>
+    <div className="flex flex-row gap-4 bg-white p-4 rounded shadow">
+      <input type="text" placeholder="Task Title" name="taskTitle" value={title} onChange={(e) => setTitle(e.target.value)} className="border p-2 rounded"/>
+      <textarea placeholder="Task Description" name="taskDescription" value={description} onChange={(e) => setDescription(e.target.value)} className="border p-2 rounded h-11"></textarea>
+      <select name="taskStatus" value={status} onChange={(e) => setStatus(e.target.value as "ready" | "inprogress" | "complete")} className="border p-2 rounded">
         <option value="ready">Ready</option>
         <option value="inprogress">In Progress</option>
         <option value="complete">Complete</option>
       </select>
-      <button onClick={handleCreate}>Create Task</button>
+      <button onClick={handleCreate} className="bg-blue-500 text-white p-2 rounded">Create Task</button>
     </div>
   )
 }
